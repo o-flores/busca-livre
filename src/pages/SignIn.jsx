23ef11ed856a7b30038f334db9957041b5b13d88
Orlando Flores <orlando_flores@id.uff.br>
2021-07-31 21:13:00 -0300

    feat: cria componente para logar usuario

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { loginAction } from '../actions';
+import Header from '../components/Header';
+import { getUserFromLocalStorage, saveUserToLocalStorage, logout } from '../services/localStorage';
+import '../styles/signin.css'
 
 class SignIn extends Component {
   constructor() {
@@ -8,7 +11,14 @@ class SignIn extends Component {
     this.state = {
       name: '',
       email: '',
+      islogged: false,
     }
+  }
+
+  componentDidMount() {
+    const user = getUserFromLocalStorage();
+    if (!user) return;
+    if (user.name) this.setState({ islogged: true })
   }
 
   handleChange = ({ target: { name, value } }) => {
@@ -18,39 +28,59 @@ class SignIn extends Component {
   handleSubmit = (event) => {
     event.preventDefault();
     const { login, history } = this.props;
-    login(this.state);
+    const { name, email } = this.state;
+    login({ name, email });
+    saveUserToLocalStorage({ name, email });
     history.push('/busca-livre')
   }
 
+  handleLogout = () => {
+    const { history } = this.props;
+    logout();
+  }
+
   render() {
-    const { name, email } = this.state;
+    const { name, email, islogged } = this.state;
     return (
-      <form onSubmit={ this.handleSubmit }>
-        <label htmlFor="name">
-          Nome:
-          <input
-            onChange={ this.handleChange }
-            value={ name }
-            type='text'
-            name="name"
-          />
-        </label>
-        <label htmlFor="name">
-          email:
-          <input
-            onChange={ this.handleChange }
-            value={ email }
-            type='text'
-            name="email"
-          />
-        </label>
-        <button
-          type='submit'
-          disabled={!(email && name)}
-        >
-          Login
-        </button>
-      </form>
+      <>
+        <Header />
+        {islogged ? (
+          <div className="logout-container">
+            <h3>Você já esta logado, caso queira se deslogar clique no botão abaixo</h3>
+            <a href="/busca-livre"><button className="user-login-logout" onClick={ this.handleLogout }>Logout</button></a>
+          </div>
+        ) : (
+          <form className="login-form" onSubmit={this.handleSubmit}>
+            <label htmlFor="name">
+              Nome:
+              <input
+                onChange={this.handleChange}
+                value={name}
+                type='text'
+                name="name"
+              />
+            </label>
+            <label htmlFor="name">
+              Email:
+              <input
+                onChange={this.handleChange}
+                value={email}
+                type='text'
+                name="email"
+              />
+            </label>
+            <div>
+            <button
+              type='submit'
+              disabled={!(email && name)}
+              className="user-login-logout"
+            >
+              Login
+            </button>
+            </div>
+          </form>
+        )}
+      </>
     )
   }
 }
