a5775a236155e33dfd56c7dc6d28719bb682273f
Orlando Flores <orlando_flores@id.uff.br>
2021-07-31 21:13:53 -0300

    feat: renderiza o usuario no header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,19 +5,23 @@ import CartButton from '../components/CartButton';
 import { MdPersonOutline } from 'react-icons/md';
 import { IoIosArrowDown } from 'react-icons/io';
 import { connect } from 'react-redux';
-import { getProductsFromStorage } from '../services/localStorage';
-import { refreshCart } from '../actions';
+import { getProductsFromStorage, getUserFromLocalStorage } from '../services/localStorage';
+import { refreshCart, loginAction } from '../actions';
 
 class Header extends React.Component {
 
   componentDidMount() {
-    const { refreshCart } = this.props;
+    const { refreshCart, login } = this.props;
     const newProducts = getProductsFromStorage();
-    refreshCart(newProducts)
+    refreshCart(newProducts);
+
+    const user = getUserFromLocalStorage();
+    if (!user) return;
+    if (user) login(user);
   }
 
   render() {
-    const { products } = this.props;
+    const { products, user } = this.props;
     return (
       <header>
         <div className='left-container'>
@@ -25,24 +29,29 @@ class Header extends React.Component {
             <a href='/busca-livre'><b><span>Busca Livre</span></b></a>
           </div>
         </div>
+        <div>
+          {user.name ? `Seja bem-vindo, ${user.name}` : 'Faça Login para aproveitar as promoções'}
+        </div>
         <nav className='right-container'>
           <span>Sobre nós</span>
-          <IoIosArrowDown/>
+          <IoIosArrowDown />
           <Link to='/signup'><MdPersonOutline className='sign-in' /></Link>
           <CartButton />
           <span className='products-counter-container'>{products.length > 0 && <span className='products-counter'>{products.length}</span>}</span>
-        </nav>    
+        </nav>
       </header>
     );
   }
 }
 
-const mapStateToProps = ({ cartReducer: { products } }) => ({
+const mapStateToProps = ({ cartReducer: { products }, userReducer: { user } }) => ({
   products,
+  user,
 })
 
 const mapDispatchToProps = (dispatch) => ({
-  refreshCart: (products) => dispatch(refreshCart(products)) 
+  refreshCart: (products) => dispatch(refreshCart(products)),
+  login: (state) => dispatch(loginAction(state)),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
