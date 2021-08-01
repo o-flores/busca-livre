d6bd53a09eab5ee25b970ddcb9d0c00337b3483e
Orlando Flores <orlando_flores@id.uff.br>
2021-07-31 21:15:12 -0300

    feat: adiciona header

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ProductCard from '../components/ProductCard';
+import Header from '../components/Header';
 
 class ProductDetails extends React.Component {
   render() {
     return (
       <>
+        <Header />
         <ProductCard props={ this.props } />
       </>
     );
